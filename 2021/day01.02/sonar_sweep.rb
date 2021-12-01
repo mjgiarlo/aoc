838b168ece4da1e46711d1285c82b15cc221e5d7
Michael J. Giarlo <mjgiarlo@stanford.edu>
2021-12-01 15:23:49 -0800

    Reformat ruby for symmetry

--- 2021/day01.02/sonar_sweep.rb
+++ 2021/day01.02/sonar_sweep.rb
@@ -1,9 +1,10 @@
-p(File
-  .readlines(File.expand_path('input.txt', __dir__), chomp: true)
-  .each_cons(3)
-  .map do |vals|
-  vals.map(&:to_i).sum
-end
-  .chunk_while do |x, y|
-    y <= x
-  end.to_a.drop(1).size)
+p(
+  File.readlines(File.expand_path('input.txt', __dir__), chomp: true)
+    .map(&:to_i)
+    .each_cons(3)
+    .map(&:sum)
+    .chunk_while { |x, y| y <= x }
+    .to_a
+    .drop(1)
+    .size
+)
