955ac5aa6e217cefa64b43cef5202827968ff254
Michael J. Giarlo <mjgiarlo@stanford.edu>
2021-12-03 09:28:30 -0800

    Solve day 3 problem 1: binary diagnostic

---
+++ 2021/day03/binary_diagnostic.rb
@@ -0,0 +1,42 @@
+class Diagnosticator
+  def initialize
+    @diagnostics = []
+  end
+
+  def register(diagnostic)
+    @diagnostics << diagnostic
+  end
+
+  def power_consumption
+    gamma * epsilon
+  end
+
+  def gamma
+    @diagnostics.map { |code| code.split('') }
+                .transpose
+                .then do |by_position|
+                  by_position.map do |digits|
+                    digits.max_by { |digit| digits.count(digit) }
+                  end
+                end
+                .join
+                .to_i(2)
+  end
+
+  # Turns out bitwise NOT isn't what I thought it was, so instead of using the
+  # Ruby unary ~ operator, XOR gamma against a string of 1 digits of the same
+  # length. That effectively flips the bits.
+  def epsilon
+    gamma ^ epsilon_calculator
+  end
+
+  def epsilon_calculator
+    ('1' * @diagnostics.first.length).to_i(2)
+  end
+end
+
+diagnosticator = Diagnosticator.new
+
+File.foreach('input.txt', chomp: true) { |diagnostic| diagnosticator.register(diagnostic) }
+
+p(diagnosticator.power_consumption)
